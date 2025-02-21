5dca040a5eebe5867e9ec17a44a48118d0ad5e48
LukaStuber <pajo024@gmail.com>
2025-02-21 22:07:31 +0100

    first proper commit. i might leave this project for a while, as i have to learn a lot. for now there's a header component with a link to youtube a page component with the text "test" and a footer with the text "copyright 2014"

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,11 +1,12 @@
 import '../styles/globals.css';
 import { Footer } from '../components/footer';
 import { Header } from '../components/header';
+import Page from './/page.jsx';
 
 export const metadata = {
     title: {
-        template: '%s | Netlify',
-        default: 'Netlify Starter'
+        template: '%s | spitedev',
+        default: 'spitedev'
     }
 };
 
@@ -15,14 +16,10 @@ export default function RootLayout({ children }) {
             <head>
                 <link rel="icon" href="/favicon.svg" sizes="any" />
             </head>
-            <body className="antialiased text-white bg-blue-900">
-                <div className="flex flex-col min-h-screen px-6 bg-grid-pattern sm:px-12">
-                    <div className="flex flex-col w-full max-w-5xl mx-auto grow">
-                        <Header />
-                        <div className="grow">{children}</div>
-                        <Footer />
-                    </div>
-                </div>
+            <body>
+                <Header/ >
+                <Page/ >
+                <Footer/ >
             </body>
         </html>
     );
